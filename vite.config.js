f4e504f90f926db9637f3816cf949c2200165b7b
vickyadri29 <vickyadri290501@gmail.com>
2023-12-07 17:31:10 +0800

    chore(title): update title's content

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.ENV_VARIABLE': JSON.stringify(process.env.ENV_VARIABLE)
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
   }
 })
