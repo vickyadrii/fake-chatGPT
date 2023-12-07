dcc0cf657d7221b9ac7608feb8034a2ba9decdb1
vickyadri29 <vickyadri290501@gmail.com>
2023-12-07 13:55:41 +0800

    feat(loading): handle loading when hit endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { PrismAsync as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [prompts, setPrompts] = useState({
     question: "",
     answer: "",
@@ -18,6 +19,8 @@ function App() {
   });
 
   const handleChatBox = async () => {
+    setIsLoading(true);
+
     await openai.chat.completions
       .create({
         messages: [
@@ -31,16 +34,14 @@ function App() {
         response_format: { type: "json_object" },
       })
       .then((res) => {
-        console.log(res.choices[0].message.content);
         setPrompts((prev) => ({
           ...prev,
           answer: JSON.parse(res.choices[0].message.content),
         }));
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
   };
-
-  console.log(keys[0]);
 
   return (
     <div className="max-w-5xl m-auto">
@@ -79,19 +80,24 @@ function App() {
                         prompts?.answer?.result}
                     </p>
                   ) : (
-                    <SyntaxHighlighter
-                      language="javascript"
-                      style={atomDark}
-                      customStyle={{
-                        color: "black",
-                      }}
-                    >
-                      {prompts?.answer?.code ??
-                        prompts?.answer?.kode ??
-                        prompts?.answer?.script ??
-                        prompts?.answer?.javascript ??
-                        prompts?.answer?.codingan}
-                    </SyntaxHighlighter>
+                    <div className="w-full">
+                      <SyntaxHighlighter
+                        language="javascript"
+                        style={atomDark}
+                        customStyle={{
+                          width: "100%",
+                        }}
+                        useInlineStyles={{
+                          innerWidth: "100%",
+                        }}
+                      >
+                        {prompts?.answer?.code ??
+                          prompts?.answer?.kode ??
+                          prompts?.answer?.script ??
+                          prompts?.answer?.javascript ??
+                          prompts?.answer?.codingan}
+                      </SyntaxHighlighter>
+                    </div>
                   )}
                 </div>
               </div>
@@ -115,9 +121,27 @@ function App() {
 
             <button
               onClick={handleChatBox}
-              className="py-3 rounded-xl bg-gray-900 hover:bg-gray-950 focus:ring focus:ring-slate-600 shadow-sm outline-none transition-all"
+              className="py-3.5 rounded-xl bg-gray-900 hover:bg-gray-950 focus:ring focus:ring-slate-600 shadow-sm outline-none transition-all"
             >
-              Send
+              {isLoading ? (
+                <div className="flex justify-center animate-spin">
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      opacity="0.2"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M10 17C10.9193 17 11.8295 16.8189 12.6788 16.4672C13.5281 16.1154 14.2997 15.5998 14.9497 14.9497C15.5998 14.2997 16.1154 13.5281 16.4672 12.6788C16.8189 11.8295 17 10.9193 17 10C17 9.08075 16.8189 8.17049 16.4672 7.32122C16.1154 6.47194 15.5998 5.70026 14.9497 5.05025C14.2997 4.40024 13.5281 3.88463 12.6788 3.53284C11.8295 3.18106 10.9193 3 10 3C8.14348 3 6.36301 3.7375 5.05025 5.05025C3.7375 6.36301 3 8.14348 3 10C3 11.8565 3.7375 13.637 5.05025 14.9497C6.36301 16.2625 8.14348 17 10 17ZM10 20C15.523 20 20 15.523 20 10C20 4.477 15.523 0 10 0C4.477 0 0 4.477 0 10C0 15.523 4.477 20 10 20Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M0 10C0 4.477 4.477 0 10 0V3C8.14348 3 6.36301 3.7375 5.05025 5.05025C3.7375 6.36301 3 8.14348 3 10H0Z"
+                      fill="#ffffff"
+                    />
+                  </svg>
+                </div>
+              ) : (
+                "Send"
+              )}
             </button>
           </div>
         </div>
